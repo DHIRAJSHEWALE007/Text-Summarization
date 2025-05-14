51d0920a75e8246785ab2ebb641995abe179b8d8
DHIRAJSHEWALE007 <dhiraj.shewa@gmail.com>
2025-05-14 17:15:08 +0530

    data ingestion and workflow

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from TextSummarizer.logging import logger
+from TextSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Hello this is logging.")
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<\n\nx============x")
+except Exception as e:
+    logger.exception(e)
+    raise e
--- src/TextSummarizer/utils/common.py
+++ src/TextSummarizer/utils/common.py
@@ -3,17 +3,17 @@ import os
 import yaml
 from TextSummarizer.logging import logger
 from ensure import ensure_annotations
-from box import config_box
+from box import ConfigBox
 from pathlib import Path
 from typing import Any
 
 @ensure_annotations
-def read_yaml(path_to_yaml:Path) -> config_box:
+def read_yaml(path_to_yaml:Path) -> ConfigBox:
     try:
         with open(path_to_yaml) as yaml_files:
             content = yaml.safe_load(yaml_files)
             logger.info(f"yaml file : {path_to_yaml} loaded successfully")
-            return config_box(content)
+            return ConfigBox(content)
     except BoxValueError:
         raise ValueError("yaml file is empty")
     except Exception as e:
