7d2328b142c449c5b521322a59d7f4cc617b58b2
DHIRAJSHEWALE007 <dhiraj.shewa@gmail.com>
2025-05-23 20:06:30 +0530

    setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 __version__ = "0.0.0"
 
 REPO_NAME = "Text-Summarization"
-AUTHOR_USER_NAME = "entbappy"
+AUTHOR_USER_NAME = "DHIRAJSHEWALE007"
 SRC_REPO = "TextSummarizer"
 AUTHOR_EMAIL = "dhiraj.shewa@gmail.com"
 
