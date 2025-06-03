1db7f9a8bb41aeb7dcbb4f1caec1a2aa4fd0b215
DHIRAJSHEWALE007 <dhiraj.shewa@gmail.com>
2025-06-03 13:34:24 +0530

    trial

--- src/TextSummarizer/components/model_evaluation.py
+++ src/TextSummarizer/components/model_evaluation.py
@@ -52,7 +52,7 @@ class ModelEvaluation:
 
         score = self.calculate_metric_on_test_ds(dataset_samsum_pt, rouge_metric, model_pegasus, tokenizer, batch_size=2, column_text='dialogue', column_summary='summary')
 
-        rouge_dict = dict((rn, score[rn].mid.fmeasure) for rn in rouge_names)
+        rouge_dict = dict((rn, score[rn]) for rn in rouge_names)
 
         df = pd.DataFrame(rouge_dict, index=['pegasus'])
         df.to_csv(self.config.metric_file_name,index=False)
